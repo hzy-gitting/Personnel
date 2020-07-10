751e60e3190ffc8d0e95848e2001356716aeb584
hzy_2018112607 <1054613842@qq.com>
2020-07-10 16:42:28 +0800

    添加了RPBLL.cs,Quit.cs和相关引用

---
+++ Personnel.BLL/QuitBLL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using personnel.DAL;
+using personnel.Model;
+
+namespace personnel.BLL
+{
+	/// <summary>
+	/// 离职BLL
+	/// 2020/7/10 16：38 通过了单元测试
+	/// hzy
+	/// </summary>
+	public class QuitBLL
+	{
+		QuitDAL quitDal = new QuitDAL();
+
+		/// <summary>
+		/// 添加离职信息（id字段为自动生成，无需手动指定）
+		/// </summary>
+		/// <param name="quit_date"></param>
+		/// <param name="quit_reason"></param>
+		/// <returns>添加成功返回true， 否则返回false</returns>
+		public bool AddQuit(DateTime quit_date, string quit_reason)
+		{
+			return quitDal.AddQuit(quit_date, quit_reason) > 0;
+		}
+
+		/// <summary>
+		/// 删除离职信息，根据id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>删除成功返回true，否则返回false</returns>
+		public bool DeleteQuitById(int id)
+		{
+			return quitDal.DeleteQuitById(id) > 0;
+		}
+		/// <summary>
+		/// 更改离职信息，根据id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="quit_date"></param>
+		/// <param name="quit_reason"></param>
+		/// <returns>更改成功返回true，否则返回false</returns>
+		public bool UpdateQuitById(int id, DateTime quit_date, string quit_reason)
+		{
+			return quitDal.UpdateQuitById(id, quit_date, quit_reason) > 0;
+		}
+
+		/// <summary>
+		/// 获取所有离职信息
+		/// </summary>
+		/// <returns>离职信息列表，若为空返回null</returns>
+		public List<Quit> GetAllQuit()
+		{
+			return quitDal.GetAll();
+		}
+
+		/// <summary>
+		/// 查询离职信息，根据id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>符合条件的离职信息,不存在则返回null</returns>
+		public Quit GetQuitById(int id)
+		{
+			return quitDal.GetQuitById(id);
+		}
+
+		/// <summary>
+		/// 查询离职信息，根据离职时间
+		/// </summary>
+		/// <param name="quit_date"></param>
+		/// <returns>符合条件的离职信息列表,若不存在符合条件的则返回null</returns>
+		public List<Quit> GetQuitByQuitDate(DateTime quit_date)
+		{
+			return quitDal.GetQuitByQuitDate(quit_date);
+		}
+
+		/// <summary>
+		/// 查询离职信息，根据离职原因
+		/// </summary>
+		/// <param name="quit_date"></param>
+		/// <returns>符合条件的离职信息列表,若不存在符合条件的则返回null</returns>
+		public List<Quit> GetQuitByQuitReason(string quit_reason)
+		{
+			return quitDal.GetQuitByQuitReason(quit_reason);
+		}
+	}
+}
---
+++ Personnel.BLL/RPBLL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using personnel.DAL;
+using personnel.Model;
+
+namespace personnel.BLL
+{
+	/// <summary>
+	/// 奖惩BLL
+	/// 2020 7/10 15：12 通过了单元测试
+	/// hzy
+	/// </summary>
+	public class RPBLL
+	{
+		RPDAL rpDal = new RPDAL();
+		/// <summary>
+		/// 添加奖惩信息（rp_id字段为自动生成，无需手动指定）
+		/// </summary>
+		/// <param name="rp_name"></param>
+		/// <param name="sal"></param>
+		/// <returns>添加成功返回true，否则返回false</returns>
+		public bool AddRP(string rp_name, int sal)
+		{
+			return rpDal.AddRP(rp_name, sal) > 0;
+		}
+		/// <summary>
+		/// 更改奖惩记录，根据id
+		/// </summary>
+		/// <param name="rp_id"></param>
+		/// <returns>更改成功返回true，否则返回false</returns>
+		public bool UpdateRPById(int rp_id, string rp_name, int sal)
+		{
+			return rpDal.UpdateRPById(rp_id, rp_name, sal) > 0;
+		}
+
+		/// <summary>
+		/// 删除奖惩记录，根据id
+		/// </summary>
+		/// <param name="rp_id"></param>
+		/// <returns>删除成功返回true，否则返回false</returns>
+		public bool DeleteRPByRPId(int rp_id)
+		{
+			return rpDal.DeleteRPByRPId(rp_id) > 0;
+		}
+		/// <summary>
+		/// 获得所有奖惩记录
+		/// </summary>
+		/// <returns>奖惩记录列表</returns>
+		public List<RP> GetAllRP()
+		{
+			return rpDal.GetAll();
+		}
+		/// <summary>
+		/// 查询奖惩记录，根据rp_id
+		/// </summary>
+		/// <param name="rp_id"></param>
+		/// <returns>符合条件的奖惩记录</returns>
+		public RP GetRPById(int rp_id)
+		{
+			return rpDal.GetRPById(rp_id);
+		}
+		/// <summary>
+		/// 查询奖惩记录，根据rp_name
+		/// </summary>
+		/// <param name="rp_name"></param>
+		/// <returns>符合条件的奖惩记录列表</returns>
+		public List<RP> GetRPByRPName(string rp_name)
+		{
+			return rpDal.GetRPByRPName(rp_name);
+		}
+	}
+}
